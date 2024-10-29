a7244cf1085fbf4f1c54d8ddd43c08b774d20758
Jason Park <jjpark987@gmail.com>
2024-10-28 20:11:08 -0700

    Create seed.py for db_scripts

---
+++ python/db_scripts/seed.py
@@ -0,0 +1,97 @@
+import json
+import os
+import re
+from dotenv import load_dotenv
+from typing import Dict
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+# Function to convert file into json data
+def parse_file(file_path) -> Dict[str, str]:
+    try:
+        # Open file as 'read'
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        # Extract the comment block
+        comment_block = re.search(r'("""[\s\S]*?""")', content)
+        if not comment_block:
+            raise ValueError("No comment block found")
+        comment = comment_block.group().strip('"""').strip()
+
+        # Define patterns for each field
+        patterns = {
+            'category': r'category:\s*(.*)',
+            'subcategory': r'subcategory:\s*(.*)',
+            'difficulty': r'difficulty:\s*(.*)',
+            # 'image_url_e1': r'image_url_e1:\s*(?:.*/)?([\w_]+_e1\.png)',
+            # 'image_url_e2': r'image_url_e2:\s*(?:.*/)?([\w_]+_e2\.png)',
+            # 'image_url_e3': r'image_url_e3:\s*(?:.*/)?([\w_]+_e3\.png)',
+            'title': r'title:\s*(.*)',
+            'description': r'description:\s*((.|\n)+?)\n\nExample',
+            'example1': r'Example 1:\s*((.|\n)+?)(Example 2:|$)',
+            'example2': r'Example 2:\s*((.|\n)+?)(Example 3:|Constraints:|$)',
+            'example3': r'Example 3:\s*((.|\n)+?)(Constraints:|$)',
+            'constraints': r'Constraints:\s*((.|\n)+?)$'
+        }
+
+        example_patterns = {
+            'input': r'Input:\s*(.*)',
+            'output': r'Output:\s*(.*)',
+            'explanation': r'Explanation:\s*((.|\n)+)'
+        }
+
+        # Extract information using regex patterns
+        parsed_data = {}
+        examples = []
+        for field, pattern in patterns.items():
+            match = re.search(pattern, comment)
+            if match:
+                # Create an example dictionary for each example content
+                example = {}
+                example_content = match.group(1)
+
+                # Loop through each part (input, output, explanation) and apply pattern
+                for part, part_pattern in example_patterns.items():
+                    part_match = re.search(part_pattern, example_content)
+                    if part_match:
+                        example[part] = part_match.group(1).strip()
+                
+                # Add the example dictionary to the examples list
+                examples.append(example)
+                
+                    # content = re.sub(r'^\s*/python/images/.+\n\n', '', content)
+                # if field.startswith('image_url'):
+                #     file_name = match.group(1)
+                #     if file_name.lower() == 'none':
+                #         parsed_data[field] = None
+                #     else:
+                #         parsed_data[field] = f'https://storage.googleapis.com/code-problem-images/{file_name}'
+                # elif field.startswith('example'):
+                #     example_content = match.group(1)
+                #     example_content = re.sub(r'^\s*/python/images/.+\n\n', '', example_content)
+                #     parsed_data[field] = example_content.strip()
+                # else:
+                #     parsed_data[field] = match.group(1)
+            else:
+                parsed_data[field] = None
+
+        # Convert to JSON and return
+        json_data = json.dumps(parsed_data, indent=4)
+        return json_data
+    
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+# Function to store this in database CRUD
+
+# Test parse.py on main.py
+if __name__ == '__main__':
+    file_path = 'python/main.py'
+    data = parse_file(file_path)
+    if data:
+        print(data)
+
+# To run script on root, `python python/db_scripts/parse_and_post.py`
--- python/main.py
+++ python/main.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python3
 
+from typing import List
+
+# time: O(n), space: O(n)
 class Solution:
-    def maxUniqueSplit(self, s: str) -> int:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         pass
-        pass
+
+print(Solution().countSubIslands(grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]))
+print('3')
+print(Solution().countSubIslands(grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]))
+print('2')
 
 """
-category: manipulations
-subcategory: strings
+category: graphs
+subcategory: grid graphs
 difficulty: medium
-image_url_e1: none
-image_url_e2: none
+image_url_e1: /python/images/count_sub_islands_e1.png
+image_url_e2: /python/images/count_sub_islands_e2.png
 image_url_e3: none
-title: Split a String Into the Max Number of Unique Substrings
+title: Path with Maximum Probability
 
 description:
-Given a string s, return the maximum number of unique substrings that the given string can be split into.
+You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's (representing land). An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
 
-You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms the original string. However, you must split the substrings such that all of them are unique.
+An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
 
-A substring is a contiguous sequence of characters within a string.
+Return the number of islands in grid2 that are considered sub-islands.
 
  
 
 Example 1:
 
-Input: s = "ababccc"
-Output: 5
-Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. Splitting like ['a', 'b', 'a', 'b', 'c', 'cc'] is not valid as you have 'a' and 'b' multiple times.
+
+Input: grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]
+Output: 3
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are three sub-islands.
 Example 2:
 
-Input: s = "aba"
-Output: 2
-Explanation: One way to split maximally is ['a', 'ba'].
-Example 3:
 
-Input: s = "aa"
-Output: 1
-Explanation: It is impossible to split the string any further.
+Input: grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]
+Output: 2 
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
  
 
 Constraints:
 
-1 <= s.length <= 16
-
-s contains only lower case English letters.
+m == grid1.length == grid2.length
+n == grid1[i].length == grid2[i].length
+1 <= m, n <= 500
+grid1[i][j] and grid2[i][j] are either 0 or 1.
 """
