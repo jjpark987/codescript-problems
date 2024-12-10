e0962bc7309fd60afbbda56cf00c4114c11c162a
Jason Park <jjpark987@gmail.com>
2024-12-09 16:57:42 -0800

    is_array_special

--- python/problems/graphs/matrixes/spiral_matrix_iii.py
+++ python/problems/graphs/matrixes/spiral_matrix_iii.py
@@ -40,8 +40,8 @@ print(Solution().spiralMatrixIII(rows = 5, cols = 6, rStart = 1, cStart = 4))
 print('[[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]')
 
 """
-category: matrix
-subcategory: traversal
+category: graphs
+subcategory: matrixes
 difficulty: medium
 image_url_e1: /python/images/spiral_matrix_iii_e1.png
 image_url_e2: /python/images/spiral_matrix_iii_e1.png
