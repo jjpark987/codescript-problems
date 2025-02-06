229fb6fcc1f527324637fdccff1f0ab7668ab828
Jason Park <jjpark987@gmail.com>
2025-02-06 13:18:39 -0800

    tuple_same_product

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ echo "PROBLEM_FILES_PATHS: $PROBLEM_FILES_PATHS"
 
 # Check if PROBLEM_FILES_PATHS has data
 echo "REACHED ENTRYPOINT.SH"
-# if [ -n "$PROBLEM_FILES_PATHS" ]; then
-#   # If there are files listed in PROBLEM_FILES_PATHS, pass them as arguments
-#   echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
-#   python python/db_scripts/parse_and_post.py --all
-# #   python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
-# else
-#   echo "❌ No problem files provided. Exiting script..."
-# fi
+if [ -n "$PROBLEM_FILES_PATHS" ]; then
+  # If there are files listed in PROBLEM_FILES_PATHS, pass them as arguments
+  echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
+  python python/db_scripts/parse_and_post.py --all
+#   python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
+else
+  echo "❌ No problem files provided. Exiting script..."
+fi
