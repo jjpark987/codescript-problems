6610b963ffdd39c7535172ecafbdebf435be077d
Jason Park <jjpark987@gmail.com>
2025-02-05 18:03:29 -0800

    Upload all problems to docker mysql container

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ echo "PROBLEM_FILES_PATHS: $PROBLEM_FILES_PATHS"
 if [ -n "$PROBLEM_FILES_PATHS" ]; then
   # If there are files listed in PROBLEM_FILES_PATHS, pass them as arguments
   echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
-  python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
+  python python/db_scripts/parse_and_post.py --all
+#   python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
 else
   echo "❌ No problem files provided. Exiting script..."
 fi
