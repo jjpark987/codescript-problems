233dff358e90da7c5fcdd3712b34a9557b19ad7d
Jason Park <jjpark987@gmail.com>
2025-02-06 18:11:54 -0800

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,6 @@
 
 echo "PROBLEM_FILES_PATHS: $PROBLEM_FILES_PATHS"
 
-# Install curl if not present
-# if ! command -v curl &> /dev/null
-# then
-#     echo "📦 Installing curl..."
-#     apt update && apt install -y curl
-# fi
-
-# # Ensure FastAPI (`app:80`) is reachable before proceeding
-# echo "🔍 Checking if FastAPI is reachable at app:80..."
-# if curl -s -o /dev/null -w "%{http_code}" http://app:80/docs | grep -q "200"; then
-#     echo "✅ FastAPI is reachable!"
-# else
-#     echo "❌ FastAPI is NOT reachable! Exiting..."
-#     exit 1
-# fi
-
 # Check if PROBLEM_FILES_PATHS has data
 if [ -n "$PROBLEM_FILES_PATHS" ]; then
   echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
@@ -26,4 +10,5 @@ else
   echo "❌ No problem files provided. Exiting script..."
 fi
 
+# To keep container running
 # tail -f /dev/null
