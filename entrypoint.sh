739b7414b48d77b1de098a014ef1fe2c5cf0c7be
Jason Park <jjpark987@gmail.com>
2025-02-06 16:57:50 -0800

    update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,8 @@ echo "PROBLEM_FILES_PATHS: $PROBLEM_FILES_PATHS"
 # Check if PROBLEM_FILES_PATHS has data
 if [ -n "$PROBLEM_FILES_PATHS" ]; then
   echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
-  python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
+  python python/db_scripts/parse_and_post.py --file "python/problems/combinatorics/counting/beautiful_subsets.py"
+#   python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
 else
   echo "❌ No problem files provided. Exiting script..."
 fi
-
-tail -f /dev/null
