016434bf12b36144e8f655b060d0d1f1da6ea09f
Jason Park <jjpark987@gmail.com>
2025-02-11 20:13:54 -0800

    Debug gha error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo "PROBLEM_FILES_PATHS: $PROBLEM_FILES_PATHS"
 
 # Check if PROBLEM_FILES_PATHS has data
 if [ -n "$PROBLEM_FILES_PATHS" ]; then
+  echo "CHECKING FOR DATABASE URL"
+  docker exec codescript-fastapi env | grep DOCKER_DATABASE_URL
   echo "📥 Processing problem files into parse_and_post.py: $PROBLEM_FILES_PATHS..."
   python python/db_scripts/parse_and_post.py --file "$PROBLEM_FILES_PATHS"
 else
