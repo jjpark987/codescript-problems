7c83e79e099f38b4206ecee1cbdca25667079d34
Jason Park <jjpark987@gmail.com>
2025-02-14 13:36:23 -0800

    delete

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Check if PROBLEM_FILES_PATHS has data
 if [[ -n "$PROBLEM_FILES_PATHS" && "$PROBLEM_FILES_PATHS" != '""' ]]; then
   echo "📥 Processing problem files into post_problems.py: $PROBLEM_FILES_PATHS..."
+  cd "$(git rev-parse --show-toplevel)" || exit 1
   python -m python.scripts.db.post_problems --file "$PROBLEM_FILES_PATHS"
 else
   echo "❌ No problem files provided. Exiting script..."
