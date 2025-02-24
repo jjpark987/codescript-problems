433684e11dafd666d9b31aa453f6bc7d62eca3ae
Jason Park <jjpark987@gmail.com>
2025-02-24 11:33:06 -0800

    Update Dockerfile, entrypoint.sh, and docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Check if PROBLEM_FILES_PATHS has data
-if [[ -n "$PROBLEM_FILES_PATHS" && "$PROBLEM_FILES_PATHS" != '""' ]]; then
+if [[ -n "$PROBLEM_FILES_PATHS" ]]; then
   echo "📥 Processing problem files into post_problems.py: $PROBLEM_FILES_PATHS..."
   python -m python.scripts.db.post_problems --file "$PROBLEM_FILES_PATHS"
 else
-  echo "❌ No problem files provided. Exiting script."
+  echo "✅ No problem files provided"
 fi
 
 # Check if IMAGE_FILES_PATHS has data
-if [[ -n "$IMAGE_FILES_PATHS" && "$IMAGE_FILES_PATHS" != '""' ]]; then
+if [[ -n "$IMAGE_FILES_PATHS" ]]; then
   echo "🖼️ Processing images into upload_images.py: $IMAGE_FILES_PATHS..."
   python -m python.scripts.db.upload_images --file "$IMAGE_FILES_PATHS"
 else
-  echo "✅ No images to upload."
+  echo "✅ No images to upload"
 fi
